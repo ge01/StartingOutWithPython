e1c92e2e170c829d50b8c531f745e96a6c03a649
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-13 09:47:29 -0600

    Variable Reassignment

---
+++ Chapter_02/program_0210.py
@@ -0,0 +1,11 @@
+# This program demonstrates variable reassignment.
+# Assign a value to the dollars variable.
+dollars = 2.75
+print('I have {}'.format(dollars), 'in my account.')
+
+# Reassign dollars so it references
+# a different value.
+dollars = 99.5
+print('But now I have {}'.format(dollars))
+
+print("But now I have " + dollars + "in my account.")
