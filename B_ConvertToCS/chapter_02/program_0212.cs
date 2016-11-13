a10d6fdf14f27d50aca8f0f834153867ab9c2280
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-13 11:13:46 -0600

    Reading Input from the Keyboard

---
+++ B_ConvertToCS/chapter_02/program_0212.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0212
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          // Get the user's first name.
+          Console.Write("Enter your first name: ");
+          string firstName = Console.ReadLine();
+
+          // Get the user's last name.
+          Console.Write("Enter your last name: ");
+          string lastName = Console.ReadLine();
+
+          // Print a greeting to the user.
+          Console.WriteLine("Hello " + firstName + " " + lastName);
+          Console.ReadLine();
+        }
+    }
+}
