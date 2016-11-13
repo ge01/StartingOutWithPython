53222cd19c523b314003d9b27c0c02a3cb7adf6e
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-13 08:48:35 -0600

    111316

---
+++ Chapter_02/program_0209.py
@@ -0,0 +1,7 @@
+# This program demonstrates a variable.
+room = 503
+print("I am staying in room number", room)
+
+print("I am staying in room number %s" % (room))
+
+print("I am staying in room number {}".format(room))
