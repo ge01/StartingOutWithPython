14d80b8f12beda11b3f9ae768dfaabe0ac8302ba
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-13 09:59:54 -0600

    Storing Strings with the str Data Type

---
+++ Chapter_02/program_0211.py
@@ -0,0 +1,8 @@
+# Create variables to reference two strings.
+first_name = 'Kathryn'
+last_name = 'Marino'
+
+# Display the values referenced by the variables
+print(first_name + " " + last_name)
+
+print('{0} " " {1}'.format(first_name,last_name))
