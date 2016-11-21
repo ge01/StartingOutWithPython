c2a64245e3f2cb1610a62d1eebfc9c11755691f2
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-20 21:40:15 -0600

    0215 PY to C# Calculating a Percentage

---
+++ B_ConvertToCS/chapter_02/program_0215.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_0215
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program gets an item's original price and
+            // calculates its sale price, with a 20% discount.
+
+            decimal PERCENTAGE = 0.2;
+
+            // Get the item's original price.
+            Console.Write("Enter the item's original price: ");
+            var originalPrice = decimal.Parse(Console.ReadLine());
+
+            // Calculate the amount of the discount.
+            var discount = originalPrice;
+
+            // Calculate the sale price.
+            var salePrice = originalPrice - discount;
+
+            // Display the sale price.
+            Console.WriteLine("The sale price is " + salePrice);
+            Console.ReadLine();
+
+        }
+    }
+}
