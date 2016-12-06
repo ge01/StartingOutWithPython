e50b6652d45c045707192e829f8eccc12f29516d
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-12-06 16:54:37 -0600

    Program that uses the input function to read two strings

---
+++ chapter_02/misc_02/p_0212_120616.py
@@ -0,0 +1,8 @@
+# Get the user's first name.
+first_name = raw_input('Enter your first name: ')
+
+# Get the user's last name.
+last_name = raw_input('Enter your last name:')
+
+# Print a greeting to the user.
+print('Hello ' + first_name + ' ' + last_name)
